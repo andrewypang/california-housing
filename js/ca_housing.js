3b4805d9bd1741d098a83eda7de77f399add2404
Andrew Pang <andrew.y.pang@gmail.com>
2019-03-17 23:28:19 -0700

    updated UI

--- js/ca_housing.js
+++ js/ca_housing.js
@@ -40,6 +40,29 @@ function initMap(){
 	//Creating a map with DOM element which is just obtained
 	map = new google.maps.Map(mapElement, mapOptions);
 
+	// Tab Panels
+	$('.tab-list').each(function(){                   // Find lists of tabs
+	  var $this = $(this);                            // Store this list
+	  var $tab = $this.find('li.active');             // Get the active list item
+	  var $link = $tab.find('a');                     // Get link from active tab
+	  var $panel = $($link.attr('href'));             // Get active panel
+
+	  $this.on('click', '.tab-control', function(e) { // When click on a tab
+	    e.preventDefault();                           // Prevent link behavior
+	    var $link = $(this),                          // Store the current link
+	        id = this.hash;                           // Get href of clicked tab 
+
+	    if (id && !$link.is('.active')) {             // If not currently active
+	      $panel.removeClass('active');               // Make panel inactive
+	      $tab.removeClass('active');                 // Make tab inactive
+
+	      $panel = $(id).addClass('active');          // Make new panel active
+	      $tab = $link.parent().addClass('active');   // Make new tab active 
+	    }
+	  });
+	});
+
+
 	// wire up the button
     var selectBox = document.getElementById('census-variable');
     google.maps.event.addDomListener(selectBox, 'change', function() {
@@ -498,4 +521,3 @@ function parseGeoJSON() {
     });
 
 }
-		
